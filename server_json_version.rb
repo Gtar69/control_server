a41f8aaf07cb15a71bb9fa057bc01d252f4e3df0
= <=>
2015-02-26 17:07:57 +0800

    redefine node dump status

--- server_json_version.rb
+++ server_json_version.rb
@@ -33,55 +33,68 @@ class ControlServer < EM::Connection
       when "registerNodeRequest"
         begin
           p "#{Time.now} register node request information from #{@ip}:#{@control_node_port}"
-          p "tell me sth"
-          p parse_data
-          p parse_data["params"]
           packages         = parse_data["params"]["packages"]
           version          = parse_data["params"]["node"]["version"]
           mac_address      = parse_data["params"]["node"]["macAddress"]
           private_ip       = parse_data["params"]["node"]["localAddress"]
           cast_port        = parse_data["params"]["streamer"]["port"]
           streamer_version = parse_data["params"]["streamer"]["version"]
+          #register servernode
           $con.query( "INSERT INTO `servernodes` (`created_at`, `updated_at`,`ip_address`,
             `control_node_port`,`cast_port`,`status`,`name`,`version`,`private_ip_add`, `packages`,`streamer_version`)
             VALUES ( '#{Time.now}', '#{Time.now}','#{@ip}','#{@control_node_port}',
             '#{cast_port}','Available','#{mac_address}','#{version}','#{private_ip}','#{packages}','#{streamer_version}')")
           handle_send("register_response")
         rescue Exception => ex
-          p "An error of type #{ex.class} happened, message is #{ex.message}"
+          p "#{Time.now} An error of type #{ex.class} happened, message is #{ex.message}"
           response = { method: "registerNodeResponse", data: {code: 400, message: "#{ex.message}"}}
+          $con.query("INSERT INTO `servernodes` (`name`,`ip_address`,`private_ip_add`,`status`)
+            VALUES ('#{mac_address}','#{@ip}','#{private_ip}','Dump in Registration')")
           send_data(response.to_json)
         end
-
       when "heartbeatRequest"
-        p "#{Time.now} heartbeat request info from #{@ip}:#{@control_node_port}"
-        @timers.pause
-        @timers = Timers::Group.new
-        @timers.every(30) do
-          p " #{Time.now} in heartbeat request => ready to disconnect"
-          self.unbind
+        begin
+          p "#{Time.now} heartbeat request info from #{@ip}:#{@control_node_port}"
+          @timers.pause
+          @timers = Timers::Group.new
+          @timers.every(30) do
+            p "#{Time.now} in heartbeat request => ready to disconnect"
+            self.unbind
+          end
+          Thread.new { @timers.wait }
+          handle_send("heartbeat_response")
+        rescue Exception => ex
+          p "#{Time.now} heartveat request failed"
         end
-        Thread.new { @timers.wait }
-        handle_send("heartbeat_response")
-
       when "prepareGameResponse"
-        #ok / fail condition
         if parse_data["data"]["code"] == 200
           p "#{Time.now} rcv prepare response from #{@ip}:#{@control_node_port}"
           user_id = parse_data["params"]["userId"]
           $con.query("UPDATE `status_checks` SET `status` = 'notify_to_play',`updated_at` = '#{Time.now}'
             WHERE `status_checks`.`id` = #{user_id}")
         else
-
+          p "#{Time.now} rcv #{parse_data["data"]["message"]} from #{@ip}:#{@control_node_port}"
+          user_id = parse_data["params"]["userId"]
+          #prepare fail handling
+          $con.query("UPDATE `servernodes`  SET `status`='Dump in Preparation', `updated_at`='#{Time.now}'
+            WHERE `servernodes`.`control_node_port` = '#{@control_node_port}'")
+          $con.query("UPDATE `status_checks` SET `status` = 'fail_prepare',`updated_at` = '#{Time.now}'
+            WHERE `status_checks`.`id` = #{user_id}")
         end
       when "playGameResponse"
-
         if parse_data["data"]["code"] == 200
           p "#{Time.now} rcv play repsponse from #{@ip}:#{@control_node_port}"
           user_id = parse_data["params"]["userId"]
-          $con.query("UPDATE `status_checks` SET `status` = 'playing_game_now',
-            `updated_at` = '#{Time.now}' WHERE `status_checks`.`id` = #{user_id}")
+          $con.query("UPDATE `status_checks` SET `status` = 'playing_game_now',`updated_at` = '#{Time.now}'
+            WHERE `status_checks`.`id` = #{user_id}")
         else
+          p "#{Time.now} rcv #{parse_data["data"]["message"]} from #{@ip}:#{@control_node_port}"
+          user_id = parse_data["params"]["userId"]
+          #play fail handling
+          $con.query("UPDATE `servernodes`  SET `status`='Dump in Playing Game', `updated_at`='#{Time.now}'
+            WHERE `servernodes`.`control_node_port` = '#{@control_node_port}'")
+          $con.query("UPDATE `status_checks` SET `status` = 'fail_play',`updated_at` = '#{Time.now}'
+            WHERE `status_checks`.`id` = #{user_id}")
         end
 
       when "stopGameResponse"
@@ -91,18 +104,27 @@ class ControlServer < EM::Connection
             `updated_at` = '#{Time.now}', `user_id` = NULL, `product_id` = NULL
             WHERE `servernodes`.`control_node_port` = #{@control_node_port}")
         else
-
+          #stop fail handling
+          p "#{Time.now} rcv stop #{parse_data["data"]["message"]} from #{@ip}:#{@control_node_port}"
+          $con.query("UPDATE `servernodes` SET `status` = 'Dump in Stop Game',
+            `updated_at` = '#{Time.now}' WHERE `servernodes`.`control_node_port` = #{@control_node_port}")
         end
       when "stopGameRequest"
-        user_id = parse_data["data"]["userId"]
-        $con.query("UPDATE `status_checks` SET `status` = 'stop_game_from_node',
-            `updated_at` = '#{Time.now}' WHERE `status_checks`.`id` = #{a}")
-        opts = {"user_id" => parase_data["data"]["userId"], "game_id" => parase_data["params"]["gameId"]}
-        handle_send("stop_game_response", opts)
+        p "#{Time.now} rcv stop request from #{@ip}:#{@control_node_port}"
+        begin
+          $con.query("UPDATE `status_checks` SET `status` = 'stop_game_from_node',
+              `updated_at` = '#{Time.now}' WHERE `status_checks`.`id` = #{user_id}")
+          opts = {"user_id" => parse_data["params"]["userId"], "game_id" => parse_data["params"]["gameId"]}
+          handle_send("stop_game_response", opts)
+        rescue Exception => ex
+          p "An error of type #{ex.class} happened, message is #{ex.message}"
+          $con.query("UPDATE `servernodes` SET `status` = 'Dump in Stop Request From Node',
+            `updated_at` = '#{Time.now}' WHERE `servernodes`.`control_node_port` = #{@control_node_port}")
+        end
       else
       end
     rescue Exception => ex
-      puts "An error of type #{ex.class} happened, message is #{ex.message}"
+      p "An error of type #{ex.class} happened, message is #{ex.message}"
     end
   end
 
@@ -124,7 +146,7 @@ class ControlServer < EM::Connection
         storage = { host: "54.176.73.176",port: 990,username: "atgamesftp",password: "atgamescloud",
           secure: { enabled: true, validation: false}, timeout: { connection: 10, operation: 15}}
         params = { userId: opts["user_id"], gameId: opts["game_id"], synchronizeRequired: opts["update_saved"],
-          storage: storage}
+          storage: storage }
         response = { method: "prepareGameRequest", params: params}
         send_data(response.to_json)
       when "handle_play_game"
@@ -140,8 +162,11 @@ class ControlServer < EM::Connection
         response = { method: "stopGameRequest", params: {userId: opts["user_id"], gameId: opts["game_id"]}}
         send_data(response.to_json)
       when "stop_game_response"
-        data = { code: 200, message: "stop game response", userId: opts["user_id"], gameId: opts["game_id"]}
-        response = { method: "stopGameResponse", data: data}
+        p "#{Time.now} send stop response to #{@ip}:#{@control_node_port}"
+        data     = { code: 200, message: "stop game response"}
+        params   = { userId: opts["user_id"], gameId: opts["game_id"]}
+        response = { method: "stopGameResponse", data: data, params: params}
+        send_data(response.to_json)
       else
       end
     rescue Exception => ex
@@ -152,7 +177,7 @@ class ControlServer < EM::Connection
   def unbind
     $con.query("DELETE FROM `servernodes` WHERE `servernodes`.`ip_address`= '#{@ip}'
       AND `servernodes`.`control_node_port`= '#{@control_node_port}' ")
-    p "-- #{@ip}:#{@control_node_port} disconnected from the control server!"
+    p "#{@ip}:#{@control_node_port} disconnected from the control server!"
   end
 
   def self.connect_hash
@@ -163,7 +188,7 @@ end
 
 
 EventMachine.run do
-  p "control server start up"
+  p "#{Time.now} control server start up"
   begin
     $con = Mysql.new 'localhost', 'chris', '12345678','Mgmt_Server_dev'
   rescue Mysql::Error => e
